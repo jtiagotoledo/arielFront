df3e8e76ce0bd1077122810d59693effc18d8b4f
jtiagotoledo <jtiagotoledo@gmail.com>
2026-07-29 12:26:45 -0300

    database v2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,30 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import { useEffect } from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import { initDatabase } from "@/database/init";
 
-export default function App(){
-  return(
+export default function App() {
+
+  useEffect(() => {
+    initDatabase();
+  }, [])
+
+  return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Beba Água!!</Text>
+      <Text style={styles.texto}>Beba Água!</Text>
     </View>
   )
 }
 
+
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#3995EE',
-    justifyContent:'center',
-    alignItems:'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#3995EE',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  texto:{
-    color:'#ffffff',
+  texto: {
+    color: '#ffffff',
   },
 });
--- src/database/init.ts
+++ src/database/init.ts
@@ -1,21 +1,39 @@
 import * as SQLite from 'expo-sqlite';
 
-export async function initDatabase() {  
+export async function initDatabase() {
     const db = await SQLite.openDatabaseAsync('arielDB.db');
 
     await db.execAsync(`
 
         CREATE TABLE IF NOT EXISTS profile (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            meta 
+            meta_ml INTEGER
         );
 
         CREATE TABLE IF NOT EXISTS ingestao (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            horario DEFAULT CURRENT_TIMESTAMP
+            qnt_ml INTEGER NOT NULL,
+            horario TEXT DEFAULT CURRENT_TIMESTAMP
         );
     `);
-
+    console.log('banco de dados arielDB criado com sucesso');
+    await buscarIngestoes();
     return db;
+}
 
+export async function buscarIngestoes() {
+    const db = await SQLite.openDatabaseAsync('arielDB.db');
+    const result = await db.getAllAsync(
+           'SELECT * FROM ingestao;'
+    );
+    console.log('buscar ingestões: ', result);
+    return result;
+}
+
+export async function addIngestao(horarioIngestao: string) {
+    const db = await SQLite.openDatabaseAsync('arielDB.db');
+
+    await db.runAsync(
+        'INSERT INTO ingestao (horario) VALUES (?);',[horarioIngestao] 
+    );
 }
