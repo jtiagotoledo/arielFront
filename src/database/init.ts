09ae1e595d121ed46760c43d41c7539744813a7c
jtiagotoledo <jtiagotoledo@gmail.com>
2026-07-29 16:15:50 -0300

    lista de ingestões

--- src/database/init.ts
+++ src/database/init.ts
@@ -30,11 +30,11 @@ export async function buscarIngestoes() {
     return result;
 }
 
-export async function addIngestao(quantidadeIngerida:number,horarioIngestao: string) {
+export async function addIngestao(quantidadeIngerida:number) {
     const db = await SQLite.openDatabaseAsync('arielDB.db');
 
     await db.runAsync(
-        'INSERT INTO ingestao (horario) VALUES (?,?);',
-        [quantidadeIngerida,horarioIngestao] 
+        'INSERT INTO ingestao (qnt_ml) VALUES (?);',
+        [quantidadeIngerida] 
     );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,45 +1,63 @@
-import { useEffect } from "react";
-import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
-import { initDatabase } from "@/database/init";
+import { useEffect, useState } from "react";
+import { View, Text, TouchableHighlight, FlatList, StyleSheet } from 'react-native';
+import { initDatabase, addIngestao, buscarIngestoes } from "@/database/init";
 
 export function HomeScreen() {
 
-  useEffect(() => {
-    initDatabase();
-  }, [])
+    const[items, setItems] = useState<{horario:string, id: number, qnt_ml:string}[]>([])
 
-  const adicionarIngestao = () => {
-    console.log('água adicionada!');
+    useEffect(() => {
+        initDatabase();
+        const todasIngestoes = buscarIngestoes();
+        //setItems(todasIngestoes)
+    }, [])
 
-  }
+    const adicionarIngestao = () => {
+        addIngestao(200);
+    }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Beba Água!</Text>
-      <TouchableHighlight
-        style={styles.botao}
-        onPress={adicionarIngestao}
-        underlayColor='#ffffff'
-      >
-        <Text>Add</Text>
-      </TouchableHighlight>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <Text style={styles.texto}>Beba Água!</Text>
+            <TouchableHighlight
+                style={styles.botao}
+                onPress={adicionarIngestao}
+                underlayColor='#ffffff'
+            >
+                <Text>Add</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+                style={styles.botao}
+                onPress={() => buscarIngestoes()}
+                underlayColor='#ffffff'
+            >
+                <Text>Buscar</Text>
+            </TouchableHighlight>
+            {/* <FlatList
+                data={ }
+                keyExtractor={ }
+
+            >
+
+            </FlatList> */}
+        </View>
+    )
 }
 
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3995EE',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  texto: {
-    color: '#ffffff',
-  },
-  botao: {
-    backgroundColor: '#ffffff',
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#3995EE',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    texto: {
+        color: '#ffffff',
+    },
+    botao: {
+        backgroundColor: '#ffffff',
+        marginTop: 24,
+    }
 });
