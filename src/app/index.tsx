83ad6d4e51cf5d296593094904bc72ee7bc62405
jtiagotoledo <jtiagotoledo@gmail.com>
2026-07-29 14:18:15 -0300

    ajustado de rota inicial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,45 +1,5 @@
-import { useEffect } from "react";
-import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
-import { initDatabase } from "@/database/init";
+import  {HomeScreen} from '../screens/HomeScreen'
 
-export default function App() {
-
-  useEffect(() => {
-    initDatabase();
-  }, [])
-
-  const adicionarIngestao = () => {
-    console.log('água adicionada!');
-
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Beba Água!</Text>
-      <TouchableHighlight
-        style={styles.botao}
-        onPress={adicionarIngestao}
-        underlayColor='#ffffff'
-      >
-        <Text>Add</Text>
-      </TouchableHighlight>
-    </View>
-  )
+export default function indexIndexRoute () {
+  return <HomeScreen/>;
 }
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3995EE',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  texto: {
-    color: '#ffffff',
-  },
-  botao: {
-    backgroundColor: '#ffffff',
-  }
-});
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
+import { initDatabase } from "@/database/init";
+
+export function HomeScreen() {
+
+  useEffect(() => {
+    initDatabase();
+  }, [])
+
+  const adicionarIngestao = () => {
+    console.log('água adicionada!');
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Beba Água!</Text>
+      <TouchableHighlight
+        style={styles.botao}
+        onPress={adicionarIngestao}
+        underlayColor='#ffffff'
+      >
+        <Text>Add</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3995EE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  texto: {
+    color: '#ffffff',
+  },
+  botao: {
+    backgroundColor: '#ffffff',
+  }
+});
