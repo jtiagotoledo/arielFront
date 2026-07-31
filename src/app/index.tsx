feb13acf94fa18efb0180c0d15469b65c474ba16
Tiago Toledo <jtiagotoledo@gmail.com>
2026-07-31 11:11:32 -0300

    adicionado notificador

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
+import { agendarNotificacao } from '@/services/notifications';
 import  {HomeScreen} from '../screens/HomeScreen'
 
-export default function indexIndexRoute () {
+export default function IndexRoute () {
+  useEffect(()=>{
+    agendarNotificacao(7,20);
+  },[])
+  
   return <HomeScreen/>;
 }
---
+++ src/services/notifications.ts
@@ -0,0 +1,58 @@
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+        shouldShowBanner: true,
+        shouldShowList: true,
+    }),
+});
+
+export async function solicitarPermissao() {
+    const { status: statusExistente } = await Notifications.getPermissionsAsync();
+    let statusFinal = statusExistente;
+
+    if (statusExistente !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        statusFinal = status;
+    }
+
+    if (statusFinal !== 'granted') {
+        console.log('Permissão de notificação negada!');
+        return false;
+    }
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('lembretes-agua',{
+            name: 'Lembretes de água',
+            importance: Notifications.AndroidImportance.HIGH,
+            vibrationPattern:[0,250,250,250],
+        });
+    }
+    return true;
+}
+
+export async function agendarNotificacao(hor_inicio:number,hor_fim:number){
+    const temPermissao = await solicitarPermissao();
+    if(!temPermissao) return;
+
+    await Notifications.cancelAllScheduledNotificationsAsync();
+
+    for(let hora = hor_inicio; hora<=hor_fim; hora++){
+        await Notifications.scheduleNotificationAsync({
+            content:{
+                title:'Ariel - Beba água!',
+                body: 'Mantenha-se hidratado...',
+                sound: true,
+            },
+            trigger:{
+                type: Notifications.SchedulableTriggerInputTypes.DAILY,
+                hour:hora,
+                minute:10,
+            },
+        });
+    }
+}
